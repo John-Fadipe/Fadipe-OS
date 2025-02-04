e4968225ad5344c5de1cd7076064e9607d744528
John-Fadipe <106629669+John-Fadipe@users.noreply.github.com>
2025-02-04 11:27:30 +0100

    Update 8. PIG.py

--- 8. PIG.py
+++ 8. PIG.py
@@ -6,9 +6,9 @@ class PIG:
 
     def roll(self):
         self.roll_dice=random.randint(1,6)
-        print(f"\nYou rolled a {roll_dice}\n")
+        print(f"\nYou rolled a {self.roll_dice}")
 
-    def play_game(self):
+    def select_player(self):
         while True:
             player=input("Enter the number of players: ")
             if player.isdigit():
@@ -16,11 +16,29 @@ class PIG:
                 if player>=2 and player<=4:
                     break
                 elif player<2 or player>4:
-                    print("Number of players can only be 2-4")
+                    print("\nNumber of players can only be 2-4")
                 else:
-                    print("Enter a valid input")
+                    print("\nEnter a valid input")
 
-            
+    def play_game(self):
+        self.select_player()
+
+        while True:
+            current_score=0
+            do_roll=input("Would you like to roll(Y)? ").upper()
+            if do_roll!="Y":
+                break
+            else:
+                self.roll()
+                if self.roll()==1:
+                    current_score=0
+                    print(f"You rolled {self.roll()}. Turn done!\n")
+                    break
+
+                else:
+                    current_score+=1
+                
+
 if __name__=="__main__":
     print("**** Welcome to the PIG Game ****\n")
     pig_game=PIG()
