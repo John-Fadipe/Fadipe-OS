9326a91daa567114c0b5bd6c9c1bd70af1c7c5e0
John-Fadipe <106629669+John-Fadipe@users.noreply.github.com>
2025-02-04 07:39:48 +0100

    Update 8. PIG.py

--- 8. PIG.py
+++ 8. PIG.py
@@ -1,4 +1,18 @@
 import random
 
 class PIG:
-    
+    def __init__(self):
+        self.total_score=0
+
+    def roll(self):
+        roll_dice=random.randint(1,6)
+
+    def play_game(self):
+        pass
+
+if __name__=="__main__":
+    print("**** Welcome to the PIG Game ****\n")
+    pig_game=PIG()
+    pig_game.play_game()
+
+    print("**** You exited the PIG Game ****\n")
