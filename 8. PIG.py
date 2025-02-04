43071c10002f20180f11d0ef6e609f1ff8003b02
John-Fadipe <106629669+John-Fadipe@users.noreply.github.com>
2025-02-04 13:04:40 +0100

    Update 8. PIG.py

--- 8. PIG.py
+++ 8. PIG.py
@@ -6,7 +6,7 @@ class PIG:
 
     def roll(self):
         self.roll_dice=random.randint(1,6)
-        print(f"\nYou rolled a {self.roll_dice}")
+        return self.roll_dice
 
     def select_player(self):
         while True:
@@ -17,31 +17,40 @@ class PIG:
                     break
                 elif player<2 or player>4:
                     print("\nNumber of players can only be 2-4")
-                else:
-                    print("\nEnter a valid input")
+            else:
+                print("\nEnter a valid input")
 
     def play_game(self):
         self.select_player()
+        self.max_score=50
+        self.current_score=0
 
-        while True:
-            current_score=0
+        while self.max_score<self.current_score:
             do_roll=input("Would you like to roll(Y)? ").upper()
             if do_roll!="Y":
                 break
             else:
-                self.roll()
-                if self.roll()==1:
-                    current_score=0
-                    print(f"You rolled {self.roll()}. Turn done!\n")
+                rolled_value=self.roll()
+                if rolled_value==1:
+                    self.current_score=0
+                    print(f"You rolled a {rolled_value}. Turn done!\n")
                     break
 
                 else:
-                    current_score+=1
-                
+                    print(f"\nYou rolled a {rolled_value}")
+                    self.current_score+=rolled_value
+
+        self.total_score+=self.current_score
+
+        print(f"Your total score is: {self.total_score}")
+        
+    def play_again(self):
+        pass
 
 if __name__=="__main__":
     print("**** Welcome to the PIG Game ****\n")
     pig_game=PIG()
     pig_game.play_game()
+    pig_game.play_again()
 
     print("\n**** You exited the PIG Game ****")
